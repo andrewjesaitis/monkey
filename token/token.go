332c411120e6cdc8b244fd762e6771c1f69ab7cd
Andrew Jesaitis <andrew@andrewjesaitis.com>
2019-08-31 11:04:24 -0700

    Define tokens

---
+++ token/token.go
@@ -0,0 +1,25 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL   = "ILLEGAL"
+	EOF       = "EOF"
+	IDENT     = "IDENT" // identifiers and literals
+	INT       = "INT"   // 123456789
+	ASSIGN    = "="
+	PLUS      = "+"
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	FUNCTION  = "FUNCTION"
+	LET       = "LET"
+)
