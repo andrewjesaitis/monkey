22c9a94c80b9ff03b9171060b4a2823d212915fb
Andrew Jesaitis <andrew@andrewjesaitis.com>
2019-11-17 10:50:07 -0800

    Add BlockStatement

--- ast/ast.go
+++ ast/ast.go
@@ -16,6 +16,23 @@ type Statement interface {
 	statementNode()
 }
 
+type BlockStatement struct {
+	Token      token.Token // the { token
+	Statements []Statement
+}
+
+func (bs *BlockStatement) statementNode()       {}
+func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 type Expression interface {
 	Node
 	expressionNode()
