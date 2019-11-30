fa566fc20c131af1770675ce04c111c479a4bfc7
Andrew Jesaitis <andrew@andrewjesaitis.com>
2019-11-30 12:15:39 -0800

    Evaluate Let Statements

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/andrewjesaitis/monkey/evaluator"
 	"github.com/andrewjesaitis/monkey/lexer"
+	"github.com/andrewjesaitis/monkey/object"
 	"github.com/andrewjesaitis/monkey/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -25,14 +27,14 @@ func Start(in io.Reader, out io.Writer) {
 		line := scanner.Text()
 		l := lexer.New(line)
 		p := parser.New(l)
-
 		program := p.ParseProgram()
+
 		if len(p.Errors()) != 0 {
 			printParserErrors(out, p.Errors())
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
