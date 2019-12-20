22e844ac4c6314b319477db448f964b856b2a0a6
Andrew Jesaitis <andrew@andrewjesaitis.com>
2019-12-20 09:49:15 -0800

    Add quote function for macro system

---
+++ evaluator/quote_unquote_test.go
@@ -0,0 +1,36 @@
+package evaluator
+
+import (
+	"testing"
+
+	"github.com/andrewjesaitis/monkey/object"
+)
+
+func TestQuote(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{`quote(5)`, `5`},
+		{`quote(5+3)`, `(5 + 3)`},
+		{`quote(foobar)`, `foobar`},
+		{`quote(foo + bar)`, `(foo + bar)`},
+	}
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		quote, ok := evaluated.(*object.Quote)
+
+		if !ok {
+			t.Fatalf("expected *object.Quote. got=%T (%+v)",
+				evaluated, evaluated)
+		}
+
+		if quote == nil {
+			t.Fatalf("quote.Node is nil")
+		}
+
+		if quote.Node.String() != tt.expected {
+			t.Errorf("not equal. got=%q, want=%q", quote.Node.String(), tt.expected)
+		}
+	}
+}
