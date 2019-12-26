263477da563e5821173e3f4e3a9169aee5f79b6d
Andrew Jesaitis <andrew@andrewjesaitis.com>
2019-12-25 21:28:14 -0800

    Add support for evaluation on unquote macro

--- evaluator/quote_unquote_test.go
+++ evaluator/quote_unquote_test.go
@@ -34,3 +34,34 @@ func TestQuote(t *testing.T) {
 		}
 	}
 }
+
+func TestUnquote(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{`quote(unquote(5))`, `5`},
+		{`quote(unquote(5+3))`, `8`},
+		{`quote(5 + unquote(3+3))`, `(5 + 6)`},
+		{`quote(unquote(4-2) + 5)`, `(2 + 5)`},
+		{`let foobar = 8; quote(foobar)`, `foobar`},
+		{`let foobar = 8; quote(unquote(foobar))`, `8`},
+	}
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		quote, ok := evaluated.(*object.Quote)
+
+		if !ok {
+			t.Fatalf("expected *object.Quote. got=%T (%+v)",
+				evaluated, evaluated)
+		}
+
+		if quote == nil {
+			t.Fatalf("quote.Node is nil")
+		}
+
+		if quote.Node.String() != tt.expected {
+			t.Errorf("not equal. got=%q, want=%q", quote.Node.String(), tt.expected)
+		}
+	}
+}
