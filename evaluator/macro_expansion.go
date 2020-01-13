bdcbe3259bcddad3b89b34f0c2000daecd5521ae
Andrew Jesaitis <andrew@andrewjesaitis.com>
2020-01-13 15:17:14 -0800

    Add expandMacros function

--- evaluator/macro_expansion.go
+++ evaluator/macro_expansion.go
@@ -26,6 +26,32 @@ func DefineMacros(program *ast.Program, env *object.Environment) {
 	}
 }
 
+func ExpandMacros(program ast.Node, env *object.Environment) ast.Node {
+	return ast.Modify(program, func(node ast.Node) ast.Node {
+		callExpression, ok := node.(*ast.CallExpression)
+		if !ok {
+			return node
+		}
+
+		macro, ok := isMacroCall(callExpression, env)
+		if !ok {
+			return node
+		}
+
+		args := quoteArgs(callExpression)
+		evalEnv := extendMacroEnv(macro, args)
+
+		evaluated := Eval(macro.Body, evalEnv)
+
+		quote, ok := evaluated.(*object.Quote)
+		if !ok {
+			panic("we only support returing AST-nodes from macros")
+		}
+
+		return quote.Node
+	})
+}
+
 func isMacroDefinition(node ast.Statement) bool {
 	letStatement, ok := node.(*ast.LetStatement)
 	if !ok {
@@ -38,6 +64,44 @@ func isMacroDefinition(node ast.Statement) bool {
 	}
 
 	return true
+}
+
+func isMacroCall(exp *ast.CallExpression, env *object.Environment) (*object.Macro, bool) {
+	identifier, ok := exp.Function.(*ast.Identifier)
+	if !ok {
+		return nil, false
+	}
+
+	obj, ok := env.Get(identifier.Value)
+	if !ok {
+		return nil, false
+	}
+
+	macro, ok := obj.(*object.Macro)
+	if !ok {
+		return nil, false
+	}
+
+	return macro, true
+}
+
+func quoteArgs(exp *ast.CallExpression) []*object.Quote {
+	args := []*object.Quote{}
+
+	for _, a := range exp.Arguments {
+		args = append(args, &object.Quote{Node: a})
+	}
+
+	return args
+}
+
+func extendMacroEnv(macro *object.Macro, args []*object.Quote) *object.Environment {
+	extended := object.NewEnclosedEvironment(macro.Env)
+
+	for idx, param := range macro.Parameters {
+		extended.Set(param.Value, args[idx])
+	}
+	return extended
 }
 
 func addMacro(stmt ast.Statement, env *object.Environment) {
